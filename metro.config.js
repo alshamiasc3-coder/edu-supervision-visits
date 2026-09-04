bfe5e7c071d16f59472e774b867e91b7ae52b2c4
alshamiasc3-coder <alshamiasc3@gmail.com>
2026-09-04 14:30:56 +0300

    Fix native Metro @ alias resolution

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,20 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
-module.exports = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
+const defaultResolveRequest = config.resolver?.resolveRequest;
+
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (moduleName.startsWith('@/')) {
+    const aliasedModule = path.resolve(__dirname, moduleName.slice(2));
+    return context.resolveRequest(context, aliasedModule, platform);
+  }
+
+  if (defaultResolveRequest) {
+    return defaultResolveRequest(context, moduleName, platform);
+  }
+
+  return context.resolveRequest(context, moduleName, platform);
+};
+
+module.exports = config;
